9235933ef11cd73983782833cb3e4b0ad82f55c3
ipeksonyildirim <ipeksonyildirim@gmail.com>
2020-11-29 00:02:41 +0300

    Set the port number from the environment variable

--- src/main/java/myDemoApp/App.java
+++ src/main/java/myDemoApp/App.java
@@ -6,12 +6,16 @@ import static spark.Spark.get;
 import static spark.Spark.port;
 import static spark.Spark.post;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
+import static spark.Spark.port;
 
 public class App {
     public String getGreeting() {
